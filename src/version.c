ded7839300259db7aff9cbfed89149033b2aad12
barbier <barbier>
2001-02-14 20:59:07 +0000

    Forgot to commit changes in mp4h 1.1.2

--- src/version.c
+++ src/version.c
@@ -8,7 +8,7 @@
 #ifndef _SRC_VERSION_C_
 #define _SRC_VERSION_C_
 
-#define MP4H_VERSION 0x101201
+#define MP4H_VERSION 0x101202
 
 typedef struct {
     const int   v_hex;
@@ -32,14 +32,14 @@ extern mp4h_version_t mp4h_version;
 #undef  _SRC_VERSION_C_AS_HEADER_
 
 mp4h_version_t mp4h_version = {
-    0x101201,
-    "1.1.1",
-    "1.1.1 (30-Aug-2000)",
-    "This is mp4h, Version 1.1.1 (30-Aug-2000)",
-    "mp4h 1.1.1 (30-Aug-2000)",
-    "mp4h/1.1.1",
-    "@(#)mp4h 1.1.1 (30-Aug-2000)",
-    "$Id: mp4h 1.1.1 (30-Aug-2000) $"
+    0x101202,
+    "1.1.2",
+    "1.1.2 (08-Nov-2000)",
+    "This is mp4h, Version 1.1.2 (08-Nov-2000)",
+    "mp4h 1.1.2 (08-Nov-2000)",
+    "mp4h/1.1.2",
+    "@(#)mp4h 1.1.2 (08-Nov-2000)",
+    "$Id: mp4h 1.1.2 (08-Nov-2000) $"
 };
 
 #endif /* _SRC_VERSION_C_AS_HEADER_ */
