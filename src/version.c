c12e14ddbd3ada7ac579b0ac9f771129d10960b6
barbier <barbier>
2001-03-24 23:50:22 +0000

    New version: 1.1a20010324

--- src/version.c
+++ src/version.c
@@ -8,7 +8,7 @@
 #ifndef _SRC_VERSION_C_
 #define _SRC_VERSION_C_
 
-#define MP4H_VERSION 0x10101315552
+#define MP4H_VERSION 0x10101315554
 
 typedef struct {
     const int   v_hex;
@@ -32,14 +32,14 @@ extern mp4h_version_t mp4h_version;
 #undef  _SRC_VERSION_C_AS_HEADER_
 
 mp4h_version_t mp4h_version = {
-    0x10101315552,
-    "1.1a20010322",
-    "1.1a20010322 (24-Mar-2001)",
-    "This is mp4h, Version 1.1a20010322 (24-Mar-2001)",
-    "mp4h 1.1a20010322 (24-Mar-2001)",
-    "mp4h/1.1a20010322",
-    "@(#)mp4h 1.1a20010322 (24-Mar-2001)",
-    "$Id: mp4h 1.1a20010322 (24-Mar-2001) $"
+    0x10101315554,
+    "1.1a20010324",
+    "1.1a20010324 (25-Mar-2001)",
+    "This is mp4h, Version 1.1a20010324 (25-Mar-2001)",
+    "mp4h 1.1a20010324 (25-Mar-2001)",
+    "mp4h/1.1a20010324",
+    "@(#)mp4h 1.1a20010324 (25-Mar-2001)",
+    "$Id: mp4h 1.1a20010324 (25-Mar-2001) $"
 };
 
 #endif /* _SRC_VERSION_C_AS_HEADER_ */
