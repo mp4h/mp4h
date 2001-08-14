9cffa14fc839cbf692a6b9c438ef3dff248ed061
barbier <barbier>
2001-08-14 22:37:22 +0000

    Bump version 1.2.0

--- src/version.c
+++ src/version.c
@@ -8,7 +8,7 @@
 #ifndef _SRC_VERSION_C_
 #define _SRC_VERSION_C_
 
-#define MP4H_VERSION 0x1010131573E
+#define MP4H_VERSION 0x102200
 
 typedef struct {
     const int   v_hex;
@@ -28,18 +28,18 @@ extern mp4h_version_t mp4h_version;
 #else /* _SRC_VERSION_C_AS_HEADER_ */
 
 #define _SRC_VERSION_C_AS_HEADER_
-#include "version.c"
+#include "src/version.c"
 #undef  _SRC_VERSION_C_AS_HEADER_
 
 mp4h_version_t mp4h_version = {
-    0x1010131573E,
-    "1.1a20010814",
-    "1.1a20010814 (14-Aug-2001)",
-    "This is mp4h, Version 1.1a20010814 (14-Aug-2001)",
-    "mp4h 1.1a20010814 (14-Aug-2001)",
-    "mp4h/1.1a20010814",
-    "@(#)mp4h 1.1a20010814 (14-Aug-2001)",
-    "$Id: mp4h 1.1a20010814 (14-Aug-2001) $"
+    0x102200,
+    "1.2.0",
+    "1.2.0 (15-Aug-2001)",
+    "This is mp4h, Version 1.2.0 (15-Aug-2001)",
+    "mp4h 1.2.0 (15-Aug-2001)",
+    "mp4h/1.2.0",
+    "@(#)mp4h 1.2.0 (15-Aug-2001)",
+    "$Id: mp4h 1.2.0 (15-Aug-2001) $"
 };
 
 #endif /* _SRC_VERSION_C_AS_HEADER_ */
